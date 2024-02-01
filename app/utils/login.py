7ef5653f1716dd7b07b1a4e05cc20e308d3170a0
Shinogm <shinogasia@gmail.com>
2024-01-31 17:02:42 -0700

    Refactor password verification logic in login.py

--- app/utils/login.py
+++ app/utils/login.py
@@ -18,14 +18,23 @@ def verify_password(password: str, email: str, token: str):
     )
     if not user_db:
         raise HTTPException(status_code=404, detail='User not found')
-
+    
+    user_db_all = mail_db.fetch_one(
+        sql='SELECT BIN_TO_UUID(id) as id, name, email FROM users WHERE email = %s',
+        params=(email,)
+    )
     try:
         import bcrypt
         if bcrypt.checkpw(password.encode('utf-8'), user_db['password'].encode('utf-8')):
         #password == user_db['password']:
-            return True
+            return {
+                'message': 'Password correct',
+                'token': user_db_all
+            }
         else:
             print(f"{user_db['password']} != {bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())}")
-            return False
+            return {
+                'message': 'Password incorrect'
+            }
     except:
         raise HTTPException(status_code=404, detail='User not found')
